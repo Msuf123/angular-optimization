9f858ab3770178ec738d37c5cdb3164a47450fea
Msuf123 <akshatmalik18t@gmail.com>
2024-06-29 21:54:15 +0530

    Opti

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { IMAGE_CONFIG, IMAGE_LOADER, ImageLoaderConfig, provideCloudinaryLoader } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),{provide:IMAGE_LOADER,useValue:(config:ImageLoaderConfig)=>{
+    console.log(config.width,'kkkkkk')
+      return `https://res.cloudinary.com/dhm03owaz/image/upload/c_fill,h_${config.width},w_${config.width}${config.src}`
+  }},{provide:IMAGE_CONFIG,useValue:{placeholderResolution:10}}, provideRouter(routes)]
 };
