627d5d6c1b4a8fdbe21364ecbf8cb7565d809950
Msuf123 <akshatmalik18t@gmail.com>
2024-06-30 17:06:35 +0530

    Done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { IMAGE_CONFIG, IMAGE_LOADER, ImageLoaderConfig, provideCloudinaryLoader } from '@angular/common';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),{provide:IMAGE_LOADER,useValue:(config:ImageLoaderConfig)=>{
     console.log(config.width,'width of the requested image')
       return `https://res.cloudinary.com/dhm03owaz/image/upload/c_fill,h_${config.width},w_${config.width}${config.src}`
-  }},{provide:IMAGE_CONFIG,useValue:{placeholderResolution:10}}, provideRouter(routes)]
+  }},{provide:IMAGE_CONFIG,useValue:{placeholderResolution:10}}, provideRouter(routes), provideClientHydration()]
 };
